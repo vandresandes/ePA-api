a1c93873cc6a2948dc9730a618d1269767dd5646
vandresandes <vandresandes@gmail.com>
2018-12-27 14:23:52 -0300

    Criação do JpaRepository da entidade TermoGeral

---
+++ src/main/java/br/gov/ba/pge/epa/api/repository/TermoGeralRepository.java
@@ -0,0 +1,9 @@
+package br.gov.ba.pge.epa.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.gov.ba.pge.epa.api.model.TermoGeral;
+
+public interface TermoGeralRepository extends JpaRepository<TermoGeral, Long>{
+
+}
