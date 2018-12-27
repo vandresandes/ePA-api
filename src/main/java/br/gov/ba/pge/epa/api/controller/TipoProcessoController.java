2b13a37f7970ceb4e36b8c9d177aadb38a04f148
vandresandes <vandresandes@gmail.com>
2018-12-27 14:22:03 -0300

    Criação do controller da entidade TipoProcesso

---
+++ src/main/java/br/gov/ba/pge/epa/api/controller/TipoProcessoController.java
@@ -0,0 +1,10 @@
+package br.gov.ba.pge.epa.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tipoprocesso")
+public class TipoProcessoController {
+
+}
