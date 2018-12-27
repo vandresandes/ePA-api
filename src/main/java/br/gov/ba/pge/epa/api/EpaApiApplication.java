5fc5401dab7af3aba8f4f0a473b3fa7e0ba83890
vandresandes <vandresandes@gmail.com>
2018-12-27 11:47:40 -0300

    Criação do projeto

---
+++ src/main/java/br/gov/ba/pge/epa/api/EpaApiApplication.java
@@ -0,0 +1,14 @@
+package br.gov.ba.pge.epa.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpaApiApplication.class, args);
+	}
+
+}
+
