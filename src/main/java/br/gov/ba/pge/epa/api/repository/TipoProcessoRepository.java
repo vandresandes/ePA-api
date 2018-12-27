bcf7f3962717b1d1096e0f3779ef305f9a8a3176
vandresandes <vandresandes@gmail.com>
2018-12-27 14:24:00 -0300

    Criação do JpaRepository da entidade TipoProcesso

---
+++ src/main/java/br/gov/ba/pge/epa/api/repository/TipoProcessoRepository.java
@@ -0,0 +1,9 @@
+package br.gov.ba.pge.epa.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.gov.ba.pge.epa.api.model.TipoProcesso;
+
+public interface TipoProcessoRepository extends JpaRepository<TipoProcesso, Long>{
+
+}
