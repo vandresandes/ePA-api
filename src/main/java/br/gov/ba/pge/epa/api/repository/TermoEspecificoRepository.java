61ed37c301ba9782c2cafaf2150b21e33a1a2b11
vandresandes <vandresandes@gmail.com>
2018-12-27 14:23:41 -0300

    Criação do JpaRepository da entidade TermoEspecifico

---
+++ src/main/java/br/gov/ba/pge/epa/api/repository/TermoEspecificoRepository.java
@@ -0,0 +1,9 @@
+package br.gov.ba.pge.epa.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.gov.ba.pge.epa.api.model.TermoEspecifico;
+
+public interface TermoEspecificoRepository extends JpaRepository<TermoEspecifico, Long>{
+
+}
