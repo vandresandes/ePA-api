fa2be14211986eb583fdcd36a31abfce431bbabf
vandresandes <vandresandes@gmail.com>
2019-01-07 11:44:21 -0300

    Criação do script para inserts da tabela Núcleo.

---
+++ src/main/java/br/gov/ba/pge/epa/api/scripts/02 - epa-nucleo-init.sql
@@ -0,0 +1,15 @@
+SET IDENTITY_INSERT tb_nuc_nucleo ON
+
+INSERT INTO tb_nuc_nucleo (nuc_id_nucleo, nuc_no_nome) VALUES 
+(1, 'NCAD'),
+(2, 'NPA'),
+(3, 'NPE'),
+(4, 'NPREV'),
+(5, 'NTCE'),
+(6, 'NLC'),
+(7, 'NSESAB'),
+(8, 'NSAS'),
+(9, 'NSSP'),
+(10, 'NPMA')
+
+SET IDENTITY_INSERT tb_nuc_nucleo OFF
