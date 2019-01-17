62f103179594e2237b5c994db45ac94a0b192f86
vandresandes <vandresandes@gmail.com>
2019-01-17 11:37:54 -0300

    Criação dos inserts

---
+++ src/main/java/br/gov/ba/pge/epa/api/scripts/09 - epa-materia-init.sql
@@ -0,0 +1,13 @@
+SET IDENTITY_INSERT tb_materia ON
+
+INSERT INTO tb_materia (id, nome) VALUES 
+
+(1,'Controle Externo'),
+(2,'Disciplinar e Sancionatório'),
+(3,'Licitações e contratos'),
+(4,'Parcerias (Convênios, Contratos de Gestão)'),
+(5,'Patrimônio Público e Meio Ambiente'),
+(6,'Pessoal'),
+(7,'Previdência')
+
+SET IDENTITY_INSERT tb_materia OFF
