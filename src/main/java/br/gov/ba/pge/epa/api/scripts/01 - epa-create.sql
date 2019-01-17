70d90323397272d0e309ddf09269f2e59813133e
vandresandes <vandresandes@gmail.com>
2019-01-17 11:37:10 -0300

    Criação da tabela origem e matéria

--- src/main/java/br/gov/ba/pge/epa/api/scripts/01 - epa-create.sql
+++ src/main/java/br/gov/ba/pge/epa/api/scripts/01 - epa-create.sql
@@ -4,11 +4,14 @@ DROP TABLE [dbo].[tb_tipo_processo]
 DROP TABLE [dbo].[tb_termo_especifico]
 DROP TABLE [dbo].[tb_termo_geral]
 DROP TABLE [dbo].[tb_documento]
+DROP TABLE [dbo].[tb_origem]
+DROP TABLE [dbo].[tb_materia]
 
 
 CREATE TABLE tb_nucleo (
 	id BIGINT IDENTITY(1,1) NOT NULL,
-	nome VARCHAR(100) UNIQUE NOT NULL
+	nome VARCHAR(100) UNIQUE NOT NULL,
+	fk_id_materia BIGINT
 );
 
 CREATE TABLE tb_tipo_processo (
@@ -42,8 +45,19 @@ CREATE TABLE tb_checklist (
 	bo_status BIT NOT NULL
 );
 
+CREATE TABLE tb_origem (
+	id BIGINT IDENTITY(1,1) NOT NULL,
+	nome VARCHAR(100) NOT NULL,
+	descricao VARCHAR(200)
+);
+
+CREATE TABLE tb_materia (
+	id BIGINT IDENTITY(1,1) NOT NULL,
+	nome VARCHAR(100) NOT NULL
+);
+
 -- CONSTRAINTS - PRIMARY KEYS
-ALTER TABLE tb_nuc_nucleo
+ALTER TABLE tb_nucleo
     ADD CONSTRAINT PK__tb_nucleo PRIMARY KEY (id);
 ALTER TABLE tb_tipo_processo
     ADD CONSTRAINT PK__tb_tipo_processo PRIMARY KEY (id);
@@ -55,16 +69,22 @@ ALTER TABLE tb_documento
     ADD CONSTRAINT PK__tb_documento PRIMARY KEY (id);
 ALTER TABLE tb_checklist
     ADD CONSTRAINT PK__tb_checklist PRIMARY KEY (id);
+ALTER TABLE tb_origem
+    ADD CONSTRAINT PK__tb_origem PRIMARY KEY (id);
+ALTER TABLE tb_materia
+    ADD CONSTRAINT PK__tb_materia PRIMARY KEY (id);
 
 -- CONSTRAINTS - FOREIGN KEYS
 -- tb_che_checklist
 ALTER TABLE tb_checklist
-    ADD CONSTRAINT FK__tb_checklist_fk_id_nucleo FOREIGN KEY (fk_id_nucleo) REFERENCES tb_nuc_nucleo(id);
+    ADD CONSTRAINT FK__tb_checklist_fk_id_nucleo FOREIGN KEY (fk_id_nucleo) REFERENCES tb_nucleo(id);
 ALTER TABLE tb_checklist
-    ADD CONSTRAINT FK__tb_checklist_fk_id_tipo_processo FOREIGN KEY (fk_id_tipo_processo) REFERENCES tb_tpp_tipo_processo(id);
+    ADD CONSTRAINT FK__tb_checklist_fk_id_tipo_processo FOREIGN KEY (fk_id_tipo_processo) REFERENCES tb_tipo_processo(id);
 ALTER TABLE tb_checklist
-    ADD CONSTRAINT FK__tb_checklist_fk_id_termo_geral FOREIGN KEY (fk_id_termo_geral) REFERENCES tb_tge_termo_geral(id);
+    ADD CONSTRAINT FK__tb_checklist_fk_id_termo_geral FOREIGN KEY (fk_id_termo_geral) REFERENCES tb_termo_geral(id);
 ALTER TABLE tb_checklist
-    ADD CONSTRAINT FK__tb_checklist_fk_id_termo_especifico FOREIGN KEY (fk_id_termo_especifico) REFERENCES tb_tes_termo_especifico(id);
+    ADD CONSTRAINT FK__tb_checklist_fk_id_termo_especifico FOREIGN KEY (fk_id_termo_especifico) REFERENCES tb_termo_especifico(id);
 ALTER TABLE tb_checklist
-    ADD CONSTRAINT FK__tb_checklist_fk_id_documento FOREIGN KEY (fk_id_documento) REFERENCES tb_doc_documento(id);
+    ADD CONSTRAINT FK__tb_checklist_fk_id_documento FOREIGN KEY (fk_id_documento) REFERENCES tb_documento(id);
+ALTER TABLE tb_nucleo
+    ADD CONSTRAINT FK__tb_nucleo_fk_id_materia FOREIGN KEY (fk_id_materia) REFERENCES tb_materia(id);
