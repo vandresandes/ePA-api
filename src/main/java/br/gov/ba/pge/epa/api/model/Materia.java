2bb6a4dc68a7f06672b8c2030a4eb2fe2fb2defa
vandresandes <vandresandes@gmail.com>
2019-01-17 11:40:19 -0300

    Criação das entidades origem e materia

---
+++ src/main/java/br/gov/ba/pge/epa/api/model/Materia.java
@@ -0,0 +1,44 @@
+package br.gov.ba.pge.epa.api.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_materia")
+public class Materia implements Serializable {
+
+	private static final long serialVersionUID = 522456354986148607L;
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull
+	@Column(name = "nome")
+	private String nome;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
