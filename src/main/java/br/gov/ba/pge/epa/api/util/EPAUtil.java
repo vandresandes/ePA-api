58341f570dbbe41e963df85b2824b7060e3c53e4
vandresandes <vandresandes@gmail.com>
2019-01-17 11:38:51 -0300

    Criação do método adicionarSeparador

--- src/main/java/br/gov/ba/pge/epa/api/util/EPAUtil.java
+++ src/main/java/br/gov/ba/pge/epa/api/util/EPAUtil.java
@@ -1,10 +1,27 @@
 package br.gov.ba.pge.epa.api.util;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.apache.commons.lang3.StringUtils;
 
 public class EPAUtil {
 
 	public static boolean isNotBlank(String value) {
 		return StringUtils.isNotBlank(value) && !value.equals("null") && !value.equals("undefined");
+	}
+
+	public static String adicionarSeparador(List<String> lista, String separador) {
+		String resultado = "";
+		String separadorAux = "";
+		for (String item : lista) {
+			resultado += separadorAux + item;
+			separadorAux = separador;
+		}
+		return resultado;
+	}
+	
+	public static Integer getValor(Optional<Integer> optional) {
+		return optional.isPresent() ? optional.get() : null;
 	}
 }
