298d3b9fb2f7485dcee17b850ecda0c2a6e6e32e
vandresandes <vandresandes@gmail.com>
2019-01-17 11:37:33 -0300

    Criação do campo materia

--- src/main/java/br/gov/ba/pge/epa/api/scripts/02 - epa-nucleo-init.sql
+++ src/main/java/br/gov/ba/pge/epa/api/scripts/02 - epa-nucleo-init.sql
@@ -1,16 +1,15 @@
 SET IDENTITY_INSERT tb_nucleo ON
 
-INSERT INTO tb_nucleo (id, nome) VALUES 
-(1,'NCAD'),
-(2,'NLC'),
-(3,'NPA'),
-(4,'NPE'),
-(5,'NPMA'),
-(6,'NPREV'),
-(7,'NSAS'),
-(8,'NSESAB'),
-(9,'NSSP'),
-(10,'NTCE')
-
+INSERT INTO tb_nucleo (id, nome, fk_id_materia) VALUES 
+(1,'NCAD',2),
+(2,'NLC',3),
+(3,'NPA',4),
+(4,'NPE',6),
+(5,'NPMA',5),
+(6,'NPREV',7),
+(7,'NSAS',3),
+(8,'NSESAB',3),
+(9,'NSSP',3),
+(10,'NTCE',1)
 
 SET IDENTITY_INSERT tb_nucleo OFF
