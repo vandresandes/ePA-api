e346a2ac6b98d73edd038ef4031239527ea1338d
vandresandes <vandresandes@gmail.com>
2019-01-24 17:01:24 -0300

    Criação do filtro por nome da matéria

--- src/main/java/br/gov/ba/pge/epa/api/controller/specification/NucleoSpecification.java
+++ src/main/java/br/gov/ba/pge/epa/api/controller/specification/NucleoSpecification.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.jpa.domain.Specification;
 
+import br.gov.ba.pge.epa.api.model.Materia;
 import br.gov.ba.pge.epa.api.model.Nucleo;
 import br.gov.ba.pge.epa.api.repository.filter.NucleoFilter;
 
@@ -29,6 +32,10 @@ public class NucleoSpecification extends BaseSpecification<Nucleo> {
 		if (filter != null) {
 			if (StringUtils.isNotBlank(filter.getNome())) {
 				predicates.add(cb.like(cb.lower(root.get("nome")), containsLowerCase(filter.getNome())));
+			}
+			if (StringUtils.isNotBlank(filter.getNomeMateria())) {
+				Join<Nucleo, Materia> joinMateria = root.join("materia", JoinType.INNER);
+				predicates.add(cb.like(cb.lower(joinMateria.get("nome")), containsLowerCase(filter.getNomeMateria())));
 			}
 		}
 		return predicates.toArray(new Predicate[predicates.size()]);
