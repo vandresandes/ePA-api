11f7ba66c5bb21fbd8f337cb1950c22cd0403906
vandresandes <vandresandes@gmail.com>
2019-01-30 10:15:44 -0300

    Inclusão da ordenação default com a coluna obrigatorio

--- src/main/java/br/gov/ba/pge/epa/api/controller/ChecklistController.java
+++ src/main/java/br/gov/ba/pge/epa/api/controller/ChecklistController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -66,7 +67,7 @@ public class ChecklistController {
 	}
 
 	@GetMapping
-	public Page<Checklist> buscarPaginado(ChecklistFilter filter, Pageable pageable) {
+	public Page<Checklist> buscarPaginado(ChecklistFilter filter, @PageableDefault( sort= {"obrigatorio"}) Pageable pageable) {
 		Specification<Checklist> specification = ChecklistSpecification.filtrar(filter);
 		Page<Checklist> resultados = repository.findAll(specification, pageable);
 		return resultados;
