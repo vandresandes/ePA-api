d71f9a2808a90a8099a1597603d7e8019aa6aa4e
vandresandes <vandresandes@gmail.com>
2019-01-30 10:17:20 -0300

    Criação da coluna condicao na tabela checklist e ajustes nos inserts

--- src/main/java/br/gov/ba/pge/epa/api/scripts/01 - epa-create.sql
+++ src/main/java/br/gov/ba/pge/epa/api/scripts/01 - epa-create.sql
@@ -14,31 +14,43 @@ DROP TABLE [dbo].[tb_sigilo_segredo_justica]
 DROP TABLE [dbo].[tb_motivo_sigilo_segredo_justica]
 
 
+CREATE TABLE tb_materia (
+	id BIGINT IDENTITY(1,1) NOT NULL,
+	nome VARCHAR(100) NOT NULL,
+	CONSTRAINT PK__tb_materia PRIMARY KEY (id)
+);
+
 CREATE TABLE tb_nucleo (
 	id BIGINT IDENTITY(1,1) NOT NULL,
 	nome VARCHAR(100) UNIQUE NOT NULL,
-	fk_id_materia BIGINT
+	fk_id_materia BIGINT,
+	CONSTRAINT PK__tb_nucleo PRIMARY KEY (id),
+	CONSTRAINT FK__tb_nucleo_fk_id_materia FOREIGN KEY (fk_id_materia) REFERENCES tb_materia(id)
 );
 
 CREATE TABLE tb_tipo_processo (
 	id BIGINT IDENTITY(1,1) NOT NULL,
-	nome VARCHAR(100) UNIQUE NOT NULL
+	nome VARCHAR(100) UNIQUE NOT NULL,
+	CONSTRAINT PK__tb_tipo_processo PRIMARY KEY (id)
 );
 
 CREATE TABLE tb_termo_geral (
 	id BIGINT IDENTITY(1,1) NOT NULL,
-	nome VARCHAR(100) UNIQUE NOT NULL
+	nome VARCHAR(100) UNIQUE NOT NULL,
+	CONSTRAINT PK__tb_termo_geral PRIMARY KEY (id)
 );
 
 CREATE TABLE tb_termo_especifico (
 	id BIGINT IDENTITY(1,1) NOT NULL,
-	nome VARCHAR(200) UNIQUE NOT NULL
+	nome VARCHAR(200) UNIQUE NOT NULL,
+	CONSTRAINT PK__tb_termo_especifico PRIMARY KEY (id)
 );
 
 CREATE TABLE tb_documento (
 	id BIGINT IDENTITY(1,1) NOT NULL,
 	nome VARCHAR(2000) NOT NULL,
-	tipo VARCHAR(20)
+	tipo VARCHAR(20),
+	CONSTRAINT PK__tb_documento PRIMARY KEY (id)
 );
 
 CREATE TABLE tb_checklist (
@@ -49,69 +61,37 @@ CREATE TABLE tb_checklist (
 	fk_id_termo_especifico BIGINT NOT NULL,
 	fk_id_documento BIGINT NOT NULL,
 	bo_obrigatorio BIT NOT NULL,
-	complexidade VARCHAR(10)
+	condicao VARCHAR(300),
+	complexidade VARCHAR(10),
+	CONSTRAINT PK__tb_checklist PRIMARY KEY (id),
+	CONSTRAINT FK__tb_checklist_fk_id_nucleo FOREIGN KEY (fk_id_nucleo) REFERENCES tb_nucleo(id),
+	CONSTRAINT FK__tb_checklist_fk_id_tipo_processo FOREIGN KEY (fk_id_tipo_processo) REFERENCES tb_tipo_processo(id),
+	CONSTRAINT FK__tb_checklist_fk_id_termo_geral FOREIGN KEY (fk_id_termo_geral) REFERENCES tb_termo_geral(id),
+	CONSTRAINT FK__tb_checklist_fk_id_termo_especifico FOREIGN KEY (fk_id_termo_especifico) REFERENCES tb_termo_especifico(id),
+	CONSTRAINT FK__tb_checklist_fk_id_documento FOREIGN KEY (fk_id_documento) REFERENCES tb_documento(id)
 );
 
 CREATE TABLE tb_origem (
 	id BIGINT IDENTITY(1,1) NOT NULL,
 	nome VARCHAR(100) NOT NULL,
-	descricao VARCHAR(200)
-);
-
-CREATE TABLE tb_materia (
-	id BIGINT IDENTITY(1,1) NOT NULL,
-	nome VARCHAR(100) NOT NULL
+	descricao VARCHAR(200),
+	CONSTRAINT PK__tb_origem PRIMARY KEY (id)
 );
 
 CREATE TABLE tb_prioridade_tramitacao (
 	id BIGINT IDENTITY(1,1) NOT NULL,
-	nome VARCHAR(100) NOT NULL
+	nome VARCHAR(100) NOT NULL,
+	CONSTRAINT PK__tb_prioridade_tramitacao PRIMARY KEY (id)
 );
 
 CREATE TABLE tb_sigilo_segredo_justica (
 	id BIGINT IDENTITY(1,1) NOT NULL,
-	nome VARCHAR(100) NOT NULL
+	nome VARCHAR(100) NOT NULL,
+	CONSTRAINT PK__tb_sigilo_segredo_justica PRIMARY KEY (id)
 );
 
 CREATE TABLE tb_motivo_sigilo_segredo_justica (
 	id BIGINT IDENTITY(1,1) NOT NULL,
-	nome VARCHAR(200) NOT NULL
+	nome VARCHAR(200) NOT NULL,
+	CONSTRAINT PK__tb_motivo_sigilo_segredo_justica PRIMARY KEY (id)
 );
-
--- CONSTRAINTS - PRIMARY KEYS
-ALTER TABLE tb_nucleo
-    ADD CONSTRAINT PK__tb_nucleo PRIMARY KEY (id);
-ALTER TABLE tb_tipo_processo
-    ADD CONSTRAINT PK__tb_tipo_processo PRIMARY KEY (id);
-ALTER TABLE tb_termo_geral
-    ADD CONSTRAINT PK__tb_termo_geral PRIMARY KEY (id);
-ALTER TABLE tb_termo_especifico
-    ADD CONSTRAINT PK__tb_termo_especifico PRIMARY KEY (id);
-ALTER TABLE tb_documento
-    ADD CONSTRAINT PK__tb_documento PRIMARY KEY (id);
-ALTER TABLE tb_checklist
-    ADD CONSTRAINT PK__tb_checklist PRIMARY KEY (id);
-ALTER TABLE tb_origem
-    ADD CONSTRAINT PK__tb_origem PRIMARY KEY (id);
-ALTER TABLE tb_materia
-    ADD CONSTRAINT PK__tb_materia PRIMARY KEY (id);
-ALTER TABLE tb_prioridade_tramitacao
-    ADD CONSTRAINT PK__tb_prioridade_tramitacao PRIMARY KEY (id);
-ALTER TABLE tb_sigilo_segredo_justica
-    ADD CONSTRAINT PK__tb_sigilo_segredo_justica PRIMARY KEY (id);
-ALTER TABLE tb_motivo_sigilo_segredo_justica
-    ADD CONSTRAINT PK__tb_motivo_sigilo_segredo_justica PRIMARY KEY (id);
-
--- CONSTRAINTS - FOREIGN KEYS
-ALTER TABLE tb_checklist
-    ADD CONSTRAINT FK__tb_checklist_fk_id_nucleo FOREIGN KEY (fk_id_nucleo) REFERENCES tb_nucleo(id);
-ALTER TABLE tb_checklist
-    ADD CONSTRAINT FK__tb_checklist_fk_id_tipo_processo FOREIGN KEY (fk_id_tipo_processo) REFERENCES tb_tipo_processo(id);
-ALTER TABLE tb_checklist
-    ADD CONSTRAINT FK__tb_checklist_fk_id_termo_geral FOREIGN KEY (fk_id_termo_geral) REFERENCES tb_termo_geral(id);
-ALTER TABLE tb_checklist
-    ADD CONSTRAINT FK__tb_checklist_fk_id_termo_especifico FOREIGN KEY (fk_id_termo_especifico) REFERENCES tb_termo_especifico(id);
-ALTER TABLE tb_checklist
-    ADD CONSTRAINT FK__tb_checklist_fk_id_documento FOREIGN KEY (fk_id_documento) REFERENCES tb_documento(id);
-ALTER TABLE tb_nucleo
-    ADD CONSTRAINT FK__tb_nucleo_fk_id_materia FOREIGN KEY (fk_id_materia) REFERENCES tb_materia(id);
