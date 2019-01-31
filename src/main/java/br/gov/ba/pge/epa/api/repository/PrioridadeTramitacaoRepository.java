24d95707ba5074f59f405c43466a6496d8b21980
vandresandes <vandresandes@gmail.com>
2019-01-31 10:52:45 -0300

    Ajustes

--- src/main/java/br/gov/ba/pge/epa/api/repository/PrioridadeTramitacaoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.gov.ba.pge.epa.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.gov.ba.pge.epa.api.model.PrioridadeTramitacao;
-
-public interface PrioridadeTramitacaoRepository extends JpaRepository<PrioridadeTramitacao, Long> {
-
-}
--- src/main/java/br/gov/ba/pge/epa/api/repository/SigiloSegredoJusticaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.gov.ba.pge.epa.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.gov.ba.pge.epa.api.model.SigiloSegredoJustica;
-
-public interface SigiloSegredoJusticaRepository extends JpaRepository<SigiloSegredoJustica, Long> {
-
-}
